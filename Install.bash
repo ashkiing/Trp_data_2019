85e5e78ed7528db742b4201cc2e119e4c839398b
ashkiing <53216737+ashkiing@users.noreply.github.com>
2024-07-13 15:34:31 +0530

    Create Install.bash

---
+++ Install.bash
@@ -0,0 +1,8 @@
+#---For downloading the required files-----------
+pip install requests tenacity
+
+#----For reading and transormation purpose------
+pip install pyspark
+
+#-----Matplotlib and Seaborn to visualize the results of the queries---
+pip install matplotlib seaborn pymysql
